80a2684761ceebc9afb5ce723c898eb81dd0640d
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-07-11 10:13:15 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="learnlet",
+    version="0.1.0",
+    author="Victor Bonjean",
+    author_email="",
+    description="",
+    py_modules=["learnlet"],
+    python_requires=">=3.8",
+    install_requires=[
+        "torch"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
